8f31c581a34c53313d160da2b7faeeea730eabb3
Ryu sichan <>
2023-05-30 23:30:27 +0900

    sidebar 정리

---
+++ app/users/layout.tsx
@@ -0,0 +1,22 @@
+// import getUsers from "../actions/getUsers";
+
+// import UserList from "./components/UserList";
+import SideBar from "../components/sidebar/SideBar";
+
+export default async function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  //   const users = await getUsers();
+
+  return (
+    // @ts-expect-error Server Component
+    <SideBar>
+      <div className="h-full">
+        {/* <UserList items={users} /> */}
+        {children}
+      </div>
+    </SideBar>
+  );
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,9 +2,14 @@
 
 import { signOut } from "next-auth/react";
 import React from "react";
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
-  return <button onClick={() => signOut()}>Hello Users!</button>;
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+  );
 };
 
 export default Users;
