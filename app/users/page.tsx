c73dcc217f5d20a1211d2e0160d01f2aa845cf5b
Ryusichan <Ryusichan@github.com>
2023-05-30 18:56:00 +0900

    login page test

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
+import { signOut } from "next-auth/react";
 import React from "react";
 
 const Users = () => {
-  return <div>Hello Users!</div>;
+  return <button onClick={() => signOut()}>Hello Users!</button>;
 };
 
 export default Users;
