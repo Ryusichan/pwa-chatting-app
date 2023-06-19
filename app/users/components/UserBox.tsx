88c23100dcb193c07fef29a4e76cbd421a8ba54e
Ryusichan <Ryusichan@github.com>
2023-06-19 18:09:50 +0900

    loading section add

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -5,6 +5,7 @@ import { User } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 
 interface UserBoxProps {
   data: User;
@@ -12,7 +13,7 @@ interface UserBoxProps {
 
 const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   const router = useRouter();
-  const [idLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = useCallback(() => {
     setIsLoading(true);
@@ -28,9 +29,11 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="
+    <>
+      {isLoading && <LoadingModal />}
+      <div
+        onClick={handleClick}
+        className="
           w-full 
           relative 
           flex 
@@ -43,17 +46,18 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
           transition
           cursor-pointer
         "
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <span className="absolute inset-0" aria-hidden="true" />
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true" />
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
